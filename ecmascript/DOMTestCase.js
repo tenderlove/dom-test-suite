3c73aabdf4925702f81952da8733a6d4b20eb373
dom-ts-4 <dom-ts-4>
2001-09-06 02:00:04 +0000

    Now supporting Mozilla 0.9.3

--- ecmascript/DOMTestCase.js
+++ ecmascript/DOMTestCase.js
@@ -10,6 +10,195 @@ PURPOSE.
 See W3C License http://www.w3.org/Consortium/Legal/ for more details.
 */
 
+  function assertTrue(descr, actual) {
+    assert(descr,actual);
+  }
+
+  function assertFalse(descr, actual) {
+    assert(descr, !actual);
+  }
+
+  function assertSize(descr, expected, actual) {
+    var actualSize;
+    actualSize = actual.length;
+    assertEquals(descr, expected, actualSize);
+  }
+ 
+  function assertEqualsCollection(descr, expected, actual) {
+    //
+    //  if they aren't the same size, they aren't equal
+    assertEquals(descr, expected.length, actual.length);
+    //
+    //  if there length is the same, then every entry in the expected list
+    //     must appear once and only once in the actual list
+    var expectedLen = expected.length;
+    var expectedValue;
+    var actualLen = actual.length;
+    var i;
+    var j;
+    var matches;
+    for(i = 0; i < expectedLen; i++) {
+        matches = 0;
+        expectedValue = expected[i];
+        for(j = 0; j < actualLen; j++) {
+            if(expectedValue == actual[j]) {
+                matches++;
+            }
+        }
+        if(matches == 0) {
+            assert(descr + ": No match found for " + expectedValue,false);
+        }
+        if(matches > 1) {
+            assert(descr + ": Multiple matches found for " + expectedValue, false);
+        }
+    }
+  }
+
+
+  function assertEqualsList(descr, expected, actual) {
+    //
+    //  if they aren't the same size, they aren't equal
+    assertEquals(descr, expected.length, actual.length);
+    //
+    var actualLen = actual.length;
+    var i;
+    for(i = 0; i < actualLen; i++) {
+        if(expected[i] != actual[i]) {
+            assertEquals(descr, expected[i], actual[i]);
+        }
+    }
+  }
+
+  function assertInstanceOf(descr, type, obj) {
+    if(type == "Attr") {
+        assertEquals(descr,2,obj.nodeType);
+        var specd = obj.specified;
+    }
+  }
+
+  function assertSame(descr, expected, actual) {
+    if(expected != actual) {
+        assertEquals(descr, expected.nodeType, actual.nodeType);
+        assertEquals(descr, expected.nodeValue, actual.nodeValue);
+    }
+  }
+
+  var factory = null;
+
+
+  function MozApplyParserAttributes(parser, attrNames, attrValues) {
+  }
+ 
+  var mozStaffDocs = new Array(); 
+
+  var docsStarted = 0;
+  var docsLoaded = 0;
+
+  function documentLoaded(e) {
+      docsLoaded++;
+  }
+
+
+  //
+  //   get a document ready for the next test
+  //
+  function MozAdvanceLoad(sourceURL) {
+    var doc = document.implementation.createDocument("","temp",null);
+    doc.load(sourceURL);
+    for(var i = 0; i <= mozStaffDocs.length; i++) {
+        if(i == mozStaffDocs.length || mozStaffDocs[i] == null) {
+            mozStaffDocs[i] = doc;
+            break;
+        }
+    }
+  }
+
+  function MozDocumentBuilder_load(sourceURL, willBeModified) {
+    if(sourceURL == 'staff.xml' && this.attrNames == null && this.attrValues == null) {
+        var doc = null;
+        for(var i = 0; i < mozStaffDocs.length; i++) {
+            doc = mozStaffDocs[i];
+            if(doc != null && doc.documentElement.nodeName == "staff") {
+                if(willBeModified) {
+                    mozStaffDocs[i] = null;
+                    MozAdvanceLoad(sourceURL);
+                }
+                return doc;
+            }
+        }
+    }
+    doc = document.implementation.createDocument("","temp",null);
+    //
+    //  never seems to fire
+    //
+    doc.addEventListener("load",documentLoaded,false);
+    //
+    //   need to add listener and wait
+    docsStarted++;
+    doc.load(sourceURL);
+    for(var i = 0; i < 5; i++) {
+        if(docsLoaded >= docsStarted) break;
+        alert("Load attempt " + docsStarted.toString() + ": Press OK continue.");
+        if(doc.documentElement.nodeName != "temp") break;
+    }
+
+    if(sourceURL == "staff.xml") {
+        if(willBeModified) {
+            MozAdvanceLoad(sourceURL);
+        }
+        else {
+            mozStaffDocs[mozStaffDocs.length] = doc;
+        }
+    }
+    return doc;
+  }
+
+   function MozDocumentBuilder_getDOMImplementation() {
+        return document.implementation;
+   }
+
+  function MozDocumentBuilder_isDOMExceptionCode(ex, code) {
+    return true;
+  }
+
+  function MozDocumentBuilder_getImplementationAttribute(attr) {
+    return false;
+  }
+
+  function MozDocumentBuilder(attrNames, attrValues) {
+    this.attrNames = attrNames;
+    this.attrValues = attrValues;
+    this.load = MozDocumentBuilder_load;
+    this.isDOMExceptionCode = MozDocumentBuilder_isDOMExceptionCode;
+    this.getDOMImplementation = MozDocumentBuilder_getDOMImplementation;
+    this.getImplementationAttribute = MozDocumentBuilder_getImplementationAttribute;
+  }
+
+  var mozDefaultBuilder = null;
+
+  function MozDocumentBuilderFactory_newDocumentBuilder(attrNames, attrValues) {
+    if(attrNames == null && attrValues == null) {
+        return mozDefaultBuilder;
+    }
+    return new MozDocumentBuilder(attrNames, attrValues);
+  }
+
+  function MozDocumentBuilderFactory() {
+    this.newDocumentBuilder = MozDocumentBuilderFactory_newDocumentBuilder;
+  }
+
+  if(navigator.appName.indexOf("Netscape") != -1) {
+    mozDefaultBuilder = new MozDocumentBuilder(null,null);
+    factory = new MozDocumentBuilderFactory();
+    //
+    //  preload a couple of staff.xml documents
+    //
+    MozAdvanceLoad("staff.xml");
+    MozAdvanceLoad("staff.xml");
+  }
+    
+
+
   function IE5ApplyParserAttributes(parser, attrNames, attrValues) {
 	if(attrNames != null) {
 	    var i;
@@ -129,77 +318,7 @@ See W3C License http://www.w3.org/Consortium/Legal/ for more details.
     this.newDocumentBuilder = IE5DocumentBuilderFactory_newDocumentBuilder;
   }
 
-  var factory = new IE5DocumentBuilderFactory();
-
-  function assertTrue(descr, actual) {
-    assert(descr,actual);
+  if(factory == null && navigator.appName.indexOf("Microsoft") != -1) {
+    factory = new IE5DocumentBuilderFactory();
   }
 
-  function assertFalse(descr, actual) {
-    assert(descr, !actual);
-  }
-
-  function assertSize(descr, expected, actual) {
-    var actualSize;
-    actualSize = actual.length;
-    assertEquals(descr, expected, actualSize);
-  }
- 
-  function assertEqualsCollection(descr, expected, actual) {
-    //
-    //  if they aren't the same size, they aren't equal
-    assertEquals(descr, expected.length, actual.length);
-    //
-    //  if there length is the same, then every entry in the expected list
-    //     must appear once and only once in the actual list
-    var expectedLen = expected.length;
-    var expectedValue;
-    var actualLen = actual.length;
-    var i;
-    var j;
-    var matches;
-    for(i = 0; i < expectedLen; i++) {
-        matches = 0;
-        expectedValue = expected[i];
-        for(j = 0; j < actualLen; j++) {
-            if(expectedValue == actual[j]) {
-                matches++;
-            }
-        }
-        if(matches == 0) {
-            assert(descr + ": No match found for " + expectedValue,false);
-        }
-        if(matches > 1) {
-            assert(descr + ": Multiple matches found for " + expectedValue, false);
-        }
-    }
-  }
-
-
-  function assertEqualsList(descr, expected, actual) {
-    //
-    //  if they aren't the same size, they aren't equal
-    assertEquals(descr, expected.length, actual.length);
-    //
-    var actualLen = actual.length;
-    var i;
-    for(i = 0; i < actualLen; i++) {
-        if(expected[i] != actual[i]) {
-            assertEquals(descr, expected[i], actual[i]);
-        }
-    }
-  }
-
-  function assertInstanceOf(descr, type, obj) {
-    if(type == "Attr") {
-        assertEquals(descr,2,obj.nodeType);
-        var specd = obj.specified;
-    }
-  }
-
-  function assertSame(descr, expected, actual) {
-    if(expected != actual) {
-        assertEquals(descr, expected.nodeType, actual.nodeType);
-        assertEquals(descr, expected.nodeValue, actual.nodeValue);
-    }
-  }
