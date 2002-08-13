8e427c6602d96ee20dba10b27690179bdcdd49c3
dom-ts-4 <dom-ts-4>
2002-08-13 04:42:08 +0000

    DOM Load-Save support

---
+++ java/junit/org/w3c/domts/level3/ls/TestXerces.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2001 World Wide Web Consortium,
+ * (Massachusetts Institute of Technology, Institut National de
+ * Recherche en Informatique et en Automatique, Keio University). All
+ * Rights Reserved. This program is distributed under the W3C's Software
+ * Intellectual Property License. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ * See W3C License http://www.w3.org/Consortium/Legal/ for more details.
+ */
+
+
+package org.w3c.domts.level3.ls;
+
+import junit.framework.*;
+import java.lang.reflect.*;
+import javax.xml.parsers.*;
+import java.util.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import org.w3c.domts.*;
+
+public class TestXerces extends TestSuite {
+
+  public static TestSuite suite() throws Exception
+  {
+    Class testClass = ClassLoader.getSystemClassLoader().loadClass("org.w3c.domts.level3.ls.alltests");
+    Constructor testConstructor = testClass.getConstructor(new Class[] { DOMTestDocumentBuilderFactory.class });
+
+    DocumentBuilderFactory xercesFactory = (DocumentBuilderFactory)
+      ClassLoader.getSystemClassLoader().loadClass("org.apache.xerces.jaxp.DocumentBuilderFactoryImpl").newInstance();
+
+    DOMTestDocumentBuilderFactory factory =
+        new JAXPDOMTestDocumentBuilderFactory(xercesFactory,
+          JAXPDOMTestDocumentBuilderFactory.getConfiguration1());
+
+
+    Object test = testConstructor.newInstance(new Object[] { factory });
+
+    return new JUnitTestSuiteAdapter((DOMTestSuite) test);
+  }
+
+
+}
+
