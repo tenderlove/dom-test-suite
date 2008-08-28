67c3d52439f1e30986dd430e991fb9b675ed2676
Aaron Patterson <aaron.patterson@gmail.com>
2008-08-27 22:50:50 -0700

    getting nokogiri to execute in the test suite

--- ruby/helper.rb
+++ ruby/helper.rb
@@ -1,6 +1,35 @@
 require 'test/unit'
+require 'nokogiri'
 
-def DOMTestCase(test_case_name)
+module DOM
+  class TestCase < Test::Unit::TestCase
+    BASE = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+    undef :default_test
+
+    # Default this to text/xml for now....
+    def content_type
+      "text/xml"
+    end
+    alias :getContentType :content_type
+
+    def preload(content_type, doc_uri, will_be_modified)
+      if ['text/html', 'application/xhtml+xml'].include?(content_type)
+        if doc_uri =~ /^staff/ || doc_uri == 'datatype_normalization'
+          raise "Not a compatible test.... #{content_type}, #{doc_uri}"
+        end
+      end
+    end
+
+    def load_document(doc_uri, will_be_modified)
+      file = File.join(BASE,'tests','level1','core','files',"#{doc_uri}.xml")
+      Nokogiri::XML.parse(File.read(file))
+    end
+  end
+end
+
+def DOMTestCase(test_case_name, &block)
   klass = test_case_name.capitalize
   raise "Already defined!" if Object.const_defined?(klass.to_sym)
+  test_klass = Class.new(DOM::TestCase, &block)
+  Object.const_set(klass.to_sym, test_klass)
 end
